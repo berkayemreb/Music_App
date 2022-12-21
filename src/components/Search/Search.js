babaddf9bfe776020d48f7fb0cc6a4f7ab3cfedf
bbogum19 <berkaye2001@hotmail.com>
2022-12-22 00:38:19 +0300

    search component's style is completed

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {View, TextInput} from 'react-native';
-
+import styles from './Search.styles'
 const Search = () => {
 
     return(
-        <View>
+        <View style={styles.container}>
             <TextInput 
             placeholder='Ara'
+            cursorColor='#b0b0b0'
             />
         </View>
     )
---
+++ src/components/Search/Search.styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        margin: 15,
+        padding: 8,
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: '#a0a0a0',
+        backgroundColor: '#f0f0f0'
+
+    }
+})
